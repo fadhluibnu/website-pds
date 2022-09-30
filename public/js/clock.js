5196e0781b6645f8ea37076a8a918e5a3845d01b
fadhluibnu <f.ibnu.a1@gmail.com>
2022-09-30 13:33:22 +0700

    Clock

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,12 +1,10 @@
 function realtimeClock() {
     var timeDisplay = document.getElementById("realtime-clock");
-    var dateString = new Date().toLocaleString("WIB", {
-        timeZone: "Asia/Jakarta",
-    });
-    var formattedString = dateString.replace(" ", " | ");
-    let name = formattedString.substr(formattedString.lastIndexOf("|") + 1);
-    let pecah = name.split(".");
-    timeDisplay.innerHTML = pecah[0] + ":" + pecah[1];
+    var dateString = new Date();
+    var hour = dateString.getHours();
+    var menit = dateString.getMinutes();
+    timeDisplay.textContent =
+        ("0" + hour).substr(-2) + ":" + ("0" + menit).substr(-2);
 }
 // start();
 setInterval(realtimeClock, 1000);
